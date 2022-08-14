b60f1584d1af8ff89d13efd63d632d221a9f39ef
Viktor <teenqq97@gmail.com>
2022-08-14 12:29:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// const menu = ['home', 'about us', 'whitepaper', 'roadmap', 'team', 'partners']
+// const swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//         el: '.swiper-pagination',
+//               clickable: true,
+//           renderBullet: function (index, className) {
+//             return '<span class="menu-text' + className + '">' + (menu[index]) + '</span>';
+//           },
+//       },
+//     direction: 'vertical',
+//     slidesPerView: 1,
+//     paginationClickable: true,
+//     spaceBetween: 30,
+//     mousewheelControl: true,
+//     parallax: true,
+//     speed: 600,
+//     eventsTarget: ".swiper-slide",
+//     breakpoints: {
+        
+//     }
+// })
